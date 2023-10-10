5e825dfcc8139a8e5b3dca7ca61fba22404b56dc
Bryan Campbell <bryanjcampbell1@gmail.com>
2023-10-10 13:26:01 -0400

    add more natspec comments to KycBeacon

--- contracts/KycBeacon.sol
+++ contracts/KycBeacon.sol
@@ -28,16 +28,16 @@ contract KycBeacon is IKycBeacon, Ownable {
 
   /// @notice Checks that the auth message signer matches the msg.sender
   /// @dev msg.sender can be spoofed for view functions so this acts as our auth check
-  modifier onlyPermitted(address _userAddress, bytes32 signedMessageHash, bytes calldata signature) {
-    address signer = ECDSA.recover(signedMessageHash, signature);
+  modifier onlyPermitted(address _userAddress, bytes32 _messageHash, bytes calldata _signature) {
+    address signer = ECDSA.recover(_messageHash, _signature);
     if ((signer != msg.sender) || (signer != _userAddress && !certifierWhitelist[signer]) )revert Unauthorized(msg.sender);
     _;
   }
 
   /// @notice User submits thier docs so they can be verified by certifiers
   /// @dev Supporiting docs uploaded to IPFS are encrypted with a key stored privately in User
-  /// @param _data a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _certType a parameter just like in doxygen (must be followed by parameter name)
+  /// @param _data Private user data being uploaded for review
+  /// @param _certType Type of certification user is requesting
   function submitSupportingDocs(bytes calldata _data, CertType _certType) public {
 
     ( string memory name, 
@@ -62,13 +62,12 @@ contract KycBeacon is IKycBeacon, Ownable {
 
   /// @notice Getter function for private user data. Accessable to certifiers and the users themselves.
   /// @dev Supporiting docs uploaded to IPFS are encrypted with a key stored privately in User
-  /// @param _userAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _signedMessageHash a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _signature a parameter just like in doxygen (must be followed by parameter name)
-  /// @return Documents the return variables of a contract’s function state variable
-  function viewUser(address _userAddress, bytes32 _signedMessageHash, bytes calldata _signature) public view onlyPermitted(_userAddress, _signedMessageHash, _signature) returns(Cert[] memory, string[] memory){
+  /// @param _userAddress Address of user who's data we are getting
+  /// @param _messageHash Hash of the auth message 
+  /// @param _signature Signature of the hashed message
+  /// @return the Private user data
+  function viewUser(address _userAddress, bytes32 _messageHash, bytes calldata _signature) public view onlyPermitted(_userAddress, _messageHash, _signature) returns(Cert[] memory, string[] memory){
 
-    //dont destructure
     User storage user = users[_userAddress];
 
     string[] memory userInfo = new string[](7);
@@ -84,7 +83,7 @@ contract KycBeacon is IKycBeacon, Ownable {
   }
 
   /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _dappAddress a parameter just like in doxygen (must be followed by parameter name)
+  /// @param _dappAddress Address of the Dapp being approved by the user
   function approveDapp(address _dappAddress) public {
     User storage user = users[msg.sender];
     Dapp storage dapp = dapps[_dappAddress];
@@ -102,18 +101,18 @@ contract KycBeacon is IKycBeacon, Ownable {
     }
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _dappAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _isApproved a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Changes the users approval status of a Dapp
+  /// @param _dappAddress Address of the Dapp
+  /// @param _isApproved Boolean where if true Dapp is approved
   function editApprovedDapp(address _dappAddress, bool _isApproved) public {
     User storage user = users[msg.sender];
     user.approvedDapps[_dappAddress] = _isApproved;
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _certifierAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _website a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _taxId a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Certifier submits private data for review by the KycBeacon team
+  /// @param _certifierAddress Address of the certifier
+  /// @param _website String of the certifier's website 
+  /// @param _taxId String of the certifier's tax id 
   function registerCertifier(address _certifierAddress, string memory _website, string memory _taxId) public {
     Certifier storage certifier = certifiers[_certifierAddress];
     certifier.website = _website;
@@ -122,9 +121,9 @@ contract KycBeacon is IKycBeacon, Ownable {
     emit RegisterCertifier(msg.sender, _website);
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _userAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _certType a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Certifier calls in order to claim a new user cert request
+  /// @param _userAddress Address of user requesting cert
+  /// @param _certType Type of certification user is requesting
   function initiate(address _userAddress, CertType _certType) public {
     require(certifierWhitelist[msg.sender], "Not whitelisted");
 
@@ -149,10 +148,10 @@ contract KycBeacon is IKycBeacon, Ownable {
     
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _userAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _certType a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _expiresOn a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Certifier creates a passing cert for user
+  /// @param _userAddress Address of user requesting cert
+  /// @param _certType Type of certification user is requesting
+  /// @param _expiresOn Date that new cert expires
   function pass(address _userAddress, CertType _certType, uint _expiresOn) public {
     require(certifierWhitelist[msg.sender], "Not whitelisted");
 
@@ -172,9 +171,9 @@ contract KycBeacon is IKycBeacon, Ownable {
     }
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _userAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _certType a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Certifier rejects the user's request for a new cert
+  /// @param _userAddress Address of user requesting cert
+  /// @param _certType Type of certification user is requesting
   function fail(address _userAddress, CertType _certType) public {
     require(certifierWhitelist[msg.sender], "Not whitelisted");
 
@@ -199,11 +198,12 @@ contract KycBeacon is IKycBeacon, Ownable {
     }
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _numberOfMonths a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _dappAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _visibilitySettings a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _isAutoWhitelist a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Handles the registration of and initial subscription for a 3rd party Dapp 
+  /// @dev Dapp must inherit from IKycBeaconConsumer
+  /// @param _numberOfMonths Initial subscription period
+  /// @param _dappAddress Address of dapp 
+  /// @param _visibilitySettings User fields that dapp requests access to 
+  /// @param _isAutoWhitelist Determines if user can be whitelisted without manual review
   function registerDapp(uint8 _numberOfMonths, address _dappAddress, Visibility[] calldata _visibilitySettings, bool _isAutoWhitelist) public payable {
     require(msg.value == uint256(_numberOfMonths) * dappSubscriptionFee, "Subscription unpaid");
     require(msg.sender == IKycBeaconConsumer(_dappAddress).kycAdmin(), "Only kycAdmin can register");
@@ -216,17 +216,17 @@ contract KycBeacon is IKycBeacon, Ownable {
     emit RegisterDapp(_dappAddress, msg.sender); 
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _dappAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @return Documents the return variables of a contract’s function state variable
+  /// @notice Getter for Dapp private data. Useful for admin of 3rd party Dapps.
+  /// @param _dappAddress Address of Dapp
+  /// @return Private Dapp data includeing subscription info
   function viewDapp(address _dappAddress) public view returns(Dapp memory){
     require(msg.sender == IKycBeaconConsumer(_dappAddress).kycAdmin(), "Only kycAdmin can view");
     return dapps[_dappAddress];
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _numberOfMonths a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _dappAddress a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Renews subscription for 3rd party Dapp
+  /// @param _numberOfMonths Subscription period
+  /// @param _dappAddress Address of Dapp
   function renewSubscription(uint8 _numberOfMonths, address _dappAddress) public payable {
     require(msg.value == uint256(_numberOfMonths) * dappSubscriptionFee, "Subscription unpaid");
     Dapp storage dapp = dapps[_dappAddress];
@@ -238,10 +238,10 @@ contract KycBeacon is IKycBeacon, Ownable {
     emit RenewSubscription(_dappAddress, expires);
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _userAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _dappAddress a parameter just like in doxygen (must be followed by parameter nam
-  /// @return sdfasfasfars 
+  /// @notice Getter for private user data matching the Dapp visibility settings
+  /// @param _userAddress Address of user
+  /// @param _dappAddress Address of Dapp
+  /// @return Private user data
   function manualReview(address _userAddress, address _dappAddress) public view returns(Cert[] memory, string[] memory){
     User storage user = users[_userAddress];
     Dapp memory dapp = dapps[_dappAddress];
@@ -262,10 +262,10 @@ contract KycBeacon is IKycBeacon, Ownable {
     return(certs, userInfo);
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _certs a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _certType a parameter just like in doxygen (must be followed by parameter nam
-  /// @return sdfasfasfars 
+  /// @notice Internal function for finding the index of a cert of a specific type
+  /// @param _certs Array of user's Certs
+  /// @param _certType Type of cert we are looking for
+  /// @return The index of the cert. If no cert is found we return the max uint.
   function _findCertType(Cert[] memory _certs, CertType _certType ) internal pure returns(uint){
     uint foundAt = type(uint256).max;
     for(uint i; i< _certs.length; i++ ){
@@ -277,10 +277,10 @@ contract KycBeacon is IKycBeacon, Ownable {
     return foundAt;
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _value a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _visibilityRequests a parameter just like in doxygen (must be followed by parameter nam
-  /// @return sdfasfasfars 
+  /// @notice Internal function used to check if a user field is visible to a Dapp based on its visibilityRequests 
+  /// @param _value User value 
+  /// @param _visibilityRequests Array specified by Dapp that determines what user data it can see
+  /// @return A boolean value representing whether that user field is visible to Dapp
   function _isVisible(Visibility _value, Visibility[] memory _visibilityRequests) internal pure returns (bool) {
     for (uint i = 0; i < _visibilityRequests.length; i++) {
         if (_visibilityRequests[i] == _value) {
@@ -291,7 +291,7 @@ contract KycBeacon is IKycBeacon, Ownable {
     return false;
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
+  /// @notice Internal function 
   /// @param _value a parameter just like in doxygen (must be followed by parameter name)
   /// @param _certs a parameter just like in doxygen (must be followed by parameter nam
   /// @return sdfasfasfars 
@@ -305,21 +305,21 @@ contract KycBeacon is IKycBeacon, Ownable {
     return false;
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _certifierAddress a parameter just like in doxygen (must be followed by parameter name)
-  /// @param _isApproved a parameter just like in doxygen (must be followed by parameter nam
+  /// @notice Admin function to (dis)approve certifier to grant user certs 
+  /// @param _certifierAddress Address of certifier
+  /// @param _isApproved A boolean that, if true, grants certifier the ability to create certs
   function editCertifierWhitelist(address _certifierAddress, bool _isApproved) public onlyOwner{
     certifierWhitelist[_certifierAddress] = _isApproved;
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _fee a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Admin function used to change the certifier fee
+  /// @param _fee New fee 
   function editCertifierFee(uint256 _fee) public onlyOwner{
     certifierFee = _fee;
   }
 
-  /// @notice Approves a Dapp to view private user fields. If possible Dapp auto-whitelists user.
-  /// @param _fee a parameter just like in doxygen (must be followed by parameter name)
+  /// @notice Admin function used to change the Dapp subscription fee
+  /// @param _fee New fee
   function editDappSubscriptionFee(uint256 _fee) public onlyOwner{
     dappSubscriptionFee = _fee;
   }
